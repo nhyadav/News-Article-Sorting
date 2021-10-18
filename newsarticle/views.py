26e2daaa767fc3e4b942fdd726d9a2a285ef5cd7
narayan <42175861+nhyadav@users.noreply.github.com>
2021-10-18 21:42:40 +0530

    Done Version 1.0

--- newsarticle/views.py
+++ newsarticle/views.py
@@ -2,43 +2,77 @@ from django.shortcuts import render
 from django.http import request
 import yaml
 import pickle
+import logging
+import time
 # Create your views here.
 
-
-
+#########logging basic configuration
+logging.basicConfig(filename="logging\\userlogging.txt",
+                    filemode='a',
+                    format='%(asctime)s %(levelname)s-%(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    level=logging.DEBUG
+                    )
+logging.info('News Articles Sorting ready for use!')
 # predict with model. 
 path = "E:\\DataScience_internship_with_ineuron\\newsarticalesorting\\newsarticlesorting\\params.yaml"
-with open(path) as data:
-    parameteras = yaml.safe_load(data)
+try:
+    with open(path, encoding='UTF-8') as data:
+        parameteras = yaml.safe_load(data)
+        logging.info("Paramters loaded successfully.")
+except Exception as ex:
+    logging.exception(ex)
 
 def model_predict(text):
-    model_path = parameteras['logistic_regression']['save_model']
-    tfidf_path = parameteras['load_data']['tfidf']
-    train_code = parameteras['train_category']
-    with open(model_path, 'rb') as data:
-        model = pickle.load(data)
-    with open(tfidf_path, 'rb') as data:
-        tfidf = pickle.load(data)
-    x_test = tfidf.transform([text])
-    predict = model.predict(x_test)
-    probability = model.predict_proba(x_test)
-    for category, id_ in train_code.items():    
-        if id_ == predict[0]:
-            predict_cateory = category
-    return predict_cateory, probability
-    
+    try:
+        model_path = parameteras['logistic_regression']['save_model']
+        tfidf_path = parameteras['load_data']['tfidf']
+        train_code = parameteras['train_category']
+        with open(model_path, 'rb') as data:
+            model = pickle.load(data)
+            logging.info("Model loaded successfully!.")
+        with open(tfidf_path, 'rb') as data:
+            tfidf = pickle.load(data)
+            logging.info("tfidf vectorization loaded successfully!.")
+        x_test = tfidf.transform([text])
+        logging.info("Text representation have done.")
+        predict = model.predict(x_test)
+        logging.info("Model has predicted successfully!.")
+        probability = model.predict_proba(x_test)
+        for category, id_ in train_code.items():    
+            if id_ == predict[0]:
+                predict_cateory = category
+        return predict_cateory, probability
+    except Exception as ex:
+        logging.exception(ex)
+def thresold_for_news(probability, result):
+    final_category = result
+    if probability >= 50:
+        return final_category
+    else:
+        final_category = 'Others'
+    return final_category
 
 def home(request):
     return render(request, "newsarticle/index.html")
 
 def predict(request):
-    if request.method == 'POST':
-        text = request.POST['text_data']
-        result, probability = model_predict(text)
-        probability = round(probability.max()*100)
-        result_final = {'category':result, 'probability': probability}
-        print(result)
-        print(probability)
+    try:
+        if request.method == 'POST':
+            text = request.POST['text_data']
+            start_time = time.time()
+            if text:
+                result, probability = model_predict(text)
+                probability = round(probability.max()*100)
+                news_category = thresold_for_news(probability, result)
+                end_time = time.time()-start_time
+                result_final = {'category': news_category, 'probability': probability, 'execution_time': end_time}
+                logging.info("Output have printed on webpage.")
+            else:
+                result_final = {'category': "Please! put some text in textbox.", 'probability':0}
+                logging.warning('Please put text in textbox.')
+    except Exception as ex:
+        logging.exception(ex)
     return render(request, "newsarticle/index.html",result_final)
 
 
