1166179db60e82cf5e1b5a451504d40a8b4d4881
narayan <42175861+nhyadav@users.noreply.github.com>
2021-10-23 16:29:46 +0530

    Deployment project

--- newsarticlesorting/settings.py
+++ newsarticlesorting/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yrlsgb_i==n7y-xwm6&0--y-anp(br(6hq29e%i)bpo5%l@064'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
