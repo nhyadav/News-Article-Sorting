365fa313250f30e41a39df28aad2e2e499d0b0ab
narayan <42175861+nhyadav@users.noreply.github.com>
2021-10-24 22:52:27 +0530

    sort the path

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import se, find_packages
+
+setup(
+    name='src',
+    version='1.0',
+    description="its a wine Q package", 
+    author="c17hawke", 
+    packages=find_packages(),
+    license="MIT"
+
+
+)
