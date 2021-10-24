5f480b9d1e81ee469cf9a5dc4edbe121deadc04c
narayan <42175861+nhyadav@users.noreply.github.com>
2021-10-24 22:47:03 +0530

    sort the path

--- newsarticlesorting/settings.py
+++ newsarticlesorting/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'patanahi@214'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['nasorting.herokuapp.com','127.0.0.1']
 
