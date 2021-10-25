536ae6aaf513904bc5276249e976193d0846046f
narayan <42175861+nhyadav@users.noreply.github.com>
2021-10-25 10:04:16 +0530

    Deployment done

--- newsarticlesorting/settings.py
+++ newsarticlesorting/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'patanahi@214'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['nasorting.herokuapp.com','127.0.0.1']
 
